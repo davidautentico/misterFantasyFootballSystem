124aaf566c41ec6e7dcf745dab5fccacd9cc885d
david rosa <davidrpe@ext.inditex.com>
2022-08-31 19:27:38 +0200

    Init

---
+++ src/main/java/com/drosa/misterfantasysystem/MisterfantasysystemApplication.java
@@ -0,0 +1,13 @@
+package com.drosa.misterfantasysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisterfantasysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisterfantasysystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drosa/misterfantasysystem/MisterfantasysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drosa.misterfantasysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisterfantasysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
